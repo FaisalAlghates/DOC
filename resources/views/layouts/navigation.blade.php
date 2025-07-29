8fd1111955a67db8e1767c5484cf90c9c7df6731
FaisalAlghates <alghatesfaisal@gmail.com>
2025-07-29 02:43:19 -0700

    Remove documentation link from navigation: delete the documentation link from the navigation bar for a cleaner interface.

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -9,12 +9,6 @@
             <span>Dashboard</span>
         </a>
         
-        <a href="{{ route('docs.index') }}" class="flex items-center space-x-2 px-4 py-2 text-white/80 hover:text-cyan-300 transition-colors font-medium group">
-            <svg class="w-5 h-5 group-hover:scale-110 transition-transform" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"></path>
-            </svg>
-            <span>Documentation</span>
-        </a>
 
         <div class="relative group">
             <button class="flex items-center space-x-2 px-4 py-2 text-white/80 hover:text-cyan-300 transition-colors font-medium">
